c458642dc9d1c2d7b9a590d2dcd42de836daeb9e
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2023-04-19 18:15:02 +0700

    connect mongodb

--- nodeServerjs/src/app.js
+++ nodeServerjs/src/app.js
@@ -1,4 +1,4 @@
-const express= require("express");
+const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
@@ -9,18 +9,19 @@ app.use(morgan("dev"))
 app.use(helmet())
 app.use(compression())
 // init db
-
-
+require('./dbs/init.mongodb')
+const { checkOverload } = require('./helpers/check.connect')
+checkOverload()
 
 // init router
-app.get('/', (req,res,next)=>{
-    const strCompress='Hello Vietnamese'
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello Vietnamese'
     return res.status(200).json({
-        message:"Welcome Vietnamese",
-        metadata:strCompress.repeat(10000)
+        message: "Welcome Vietnamese",
+        metadata: strCompress.repeat(10000)
     })
 })
 // handling error
 
 
-module.exports= app;
+module.exports = app;
---
+++ nodeServerjs/src/helpers/check.connect.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const os = require('os')
+const process = require('process')
+
+
+const countConnect = () => {
+    const numberConnection = mongoose.connections.length
+    console.log(`Number of connections::${numberConnection}`)
+}
+
+const checkOverload = () => {
+    const numberConnection = mongoose.connections.length
+    const numberCores = os.cpus().length
+    const memoryUsage = process.memoryUsage().rss;
+    const maxConnect = numberCores * 5
+    setInterval(() => {
+        console.log(`memory usage:: ${memoryUsage / 1024 / 1024} MB`)
+        if (numberConnection > maxConnect)
+            console.log(`Connection overload detected`)
+    }, 5000);
+
+}
+module.exports = {
+    countConnect,
+    checkOverload
+}
