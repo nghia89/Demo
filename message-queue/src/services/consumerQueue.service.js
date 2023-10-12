d64eeb8f9c7157e874abf7d83a76e8103f205ca2
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2023-10-12 22:33:16 +0700

    update case Queue error

--- message-queue/src/services/consumerQueue.service.js
+++ message-queue/src/services/consumerQueue.service.js
@@ -4,10 +4,10 @@ const {
     consumerQueue,
     connectToRabbitMQ
 } = require('../dbs/init.rabbit')
-const log = console.log;
-console.log = function () {
-    log.apply(console, [new Date()].concat(arguments));
-}
+// const log = console.log;
+// console.log = function () {
+//     log.apply(console, [new Date()].concat(arguments));
+// }
 
 const messagesService = {
     consumerToQueue: async (queueName) => {
@@ -22,10 +22,31 @@ const messagesService = {
         try {
             const { channel, connect } = await connectToRabbitMQ()
             const notiQueue = 'notificationQueueProcess'
+
+            //1. case timeout
+            // setTimeout(() => {
+            //     channel.consume(notiQueue, msg => {
+            //         console.log('NotificationQueue successfully:', msg.content.toString())
+            //         channel.ack(msg)
+            //     })
+            // }, 15000)
+
+            //2. case failed
+
             channel.consume(notiQueue, msg => {
-                console.log('NotificationQueue successfully:', msg.content.toString())
-                channel.ack(msg)
+                try {
+                    const number = Math.random();
+                    console.log('number:', number)
+                    if (number < 0.8)
+                        throw new Error('Send notification failed: ' + number)
+                    console.log('NotificationQueue successfully:', msg.content.toString())
+                    channel.ack(msg)
+                } catch (error) {
+                    channel.nack(msg, false, false)
+                }
+
             })
+
         } catch (error) {
             console.log(error)
         }
