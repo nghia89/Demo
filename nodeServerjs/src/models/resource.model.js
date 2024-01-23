b0d96eba5d3b2e1e8a767c1f69a1724a40d92f10
Nguyen Thanh Nghia <hangnghia11089@gmail.com>
2024-01-23 21:35:05 +0700

    model phân quyền

---
+++ nodeServerjs/src/models/resource.model.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { model, Schema } = require('mongoose')
+
+const DOCUMENT_NAME = 'Resource'
+const COLLECTION_NAME = 'Resources'
+
+const resourceSchema = new Schema({
+    res_name: { type: String, required: true },//profile
+    res_slug: { type: String, required: true },//00001
+    res_description: { type: String, default: '' },
+
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+})
+
+module.exports = model(DOCUMENT_NAME, resourceSchema)
